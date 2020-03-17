2f0a82c3c599efbdf3fd05936b74274776fd4442
sreedevi <sreedevi@test.com>
2020-03-17 18:19:43 +0530

    updated index

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
-<?php
-echo "started";
+<html>
+<head>
+	<title>Cutomer Portal</title>
+</head>
+<body>
+	<form method="POST" action="module/save.php">
+		<label>Customer Name: </label><input type="text" name="customerName" required>
+		<label>Customer Code: </label><input type="text" name="customerCode" required>
+		<input type="submit" value="Save" name="saveCustomer">
+	</form>
+	<?php 
+		if(isset($_GET['action']) && $_GET['action'] == 'success') {
+			echo "saved successfully";
+		}
+	?>
+</body>
+</html>
